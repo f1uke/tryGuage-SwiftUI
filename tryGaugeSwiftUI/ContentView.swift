d55dd1d86db71dd4c7e5766f975318c3258fb250
Fluke <lingfluke@gmail.com>
2020-10-06 11:00:52 +0700

    Initial Commit

---
+++ tryGaugeSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tryGaugeSwiftUI
+//
+//  Created by Fluke on 6/10/2563 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
